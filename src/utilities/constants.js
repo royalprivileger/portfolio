bd8de6cd05eaa6b3d5018471403363644531d7bc
royalprivileger <sharmashivendra7541@gmail.com>
2025-06-25 12:57:37 +0530

    Added SEO meta tags and thumbnail image

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -4,22 +4,27 @@ import * as yp from 'yup';
 
  export const FormSchema = yp.object().shape({
   firstName: yp.string()
+    .transform((value) => value.trim())
     .min(2, 'Firstname must be at least 2 characters')
     .max(20, 'Firstname cannot exceed 20 characters')
-    .matches(/^[A-Za-z]+$/, 'First name can only contain letters')
+    .matches(/^[A-Za-z ]+$/, 'First name can only contain letters')
     .required('First name is required'),
   lastName: yp.string()
+    .transform((value) => value.trim())
     .min(2, 'Lastname must be at least 2 characters')
     .max(20, 'Lastname cannot exceed 20 characters')
-    .matches(/^[A-Za-z]+$/, 'Lastname can only contain letters')
+    .matches(/^[A-Za-z ]+$/, 'Lastname can only contain letters')
     .required('Lastname is required'),
   email: yp.string()
+    .transform((value) => value.trim())
     .email('Invalid email format')
     .required('Email is required'),
   phone: yp.string()
+    .transform((value) => value.trim())
     .matches(/^\d{10}$/, 'Phone must be 10 digits')
     .optional(),
   textMessage: yp.string()
+    .transform((value) => value.trim())
     .max(100, 'Text message cannot exceed 100 characters')
     .matches(/^[a-zA-Z0-9 \\/_@.]+$/, 'Message can only contain letters')
     .required('Message is required'),
