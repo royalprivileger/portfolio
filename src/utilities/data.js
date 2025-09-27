dd766dfda4a4ae35aba71fb3f9006fc580e493f6
royalprivileger <sharmashivendra7541@gmail.com>
2025-09-27 15:59:09 +0530

    updated recent details

--- src/utilities/data.js
+++ src/utilities/data.js
@@ -1,37 +1,49 @@
-{/*This file is for keeping the static data which are going to be used in whole project*/}
+{
+  /*This file is for keeping the static data which are going to be used in whole project*/
+}
 
 export const timelineItems = [
-    {
-      title: "SDE - I",
-      date: "Jio Platforms LTD",
-      showDate: "Mar, 2023 - Jul, 2024",
-      description: "Click to Expand",
-      detailedDescription: "Developed and maintained multiple enterprise web apps using React, Javascript, HTML/CSS, K2(Low-Code No-Code Solution) and SQL. Improved performance, integrated APIs, and streamlined UI workflows in collaboration with cross-functional teams.",
-    },
-    {
-      title: "Assistant Manager",
-      date: "Jio Platforms LTD",
-      showDate: "Aug, 2021 - Mar, 2023",
-      description: "Click to Expand",
-      detailedDescription: "Led UAT and manual testing for JioMeet Web and macOS apps, managing test cycles, reporting bugs, and ensuring stable releases through close collaboration with developers.",
-    },
-    {
-      title: "Bachelor of Engineering - EXTC",
-      date: "Pillai College of Engineering, Mumbai University",
-      showDate: "Aug, 2017 - Jun, 2021",
-      description: "Click to Expand",
-      detailedDescription:
-        "Completed Bachelor's in Electronics and Telecommunication Engineering with hands-on experience in projects involving IoT, embedded systems, and Python-based automation.",
-    },
-    {
-      title: "Internship - BSNL Satellite Training",
-      date: "Summer 2020",
-      showDate: "Jun, 2020",
-      description: "Click to Expand",
-      detailedDescription:
-        "Underwent a 15 days-long training at BSNL, where I learned about satellite communication, signal transmission processes, and real-world telecom infrastructure.",
-    },
-  ];
+  {
+    title: "SDE - I (K2 Five Frontend Developer)",
+    company: "LTIMindtree (Contractor via Advance Career Solutions)",
+    showDate: "Aug 2025 - Present",
+    description: "Click to Expand",
+    detailedDescription:
+      "Working as a SDE Nintex K2 frontend developer for LTIMindtree (on payroll with Advance Career Solutions). Developing and maintaining enterprise webpages/forms using Nintex K2 (low code - no code tool). Implementing multiple K2 workflows to improve throughput and UI workflows in collaboration with cross-functional teams.",
+  },
+  {
+    title: "SDE - I",
+    date: "Jio Platforms LTD",
+    showDate: "Mar, 2023 - Jul, 2024",
+    description: "Click to Expand",
+    detailedDescription:
+      "Developed and maintained multiple enterprise web apps using React, Javascript, HTML/CSS, K2(Low-Code No-Code Solution) and SQL. Improved performance, integrated APIs, and streamlined UI workflows in collaboration with cross-functional teams.",
+  },
+  {
+    title: "Assistant Manager",
+    date: "Jio Platforms LTD",
+    showDate: "Aug, 2021 - Mar, 2023",
+    description: "Click to Expand",
+    detailedDescription:
+      "Led UAT and manual testing for JioMeet Web and macOS apps, managing test cycles, reporting bugs, and ensuring stable releases through close collaboration with developers.",
+  },
+  {
+    title: "Bachelor of Engineering - EXTC",
+    date: "Pillai College of Engineering, Mumbai University",
+    showDate: "Aug, 2017 - Jun, 2021",
+    description: "Click to Expand",
+    detailedDescription:
+      "Completed Bachelor's in Electronics and Telecommunication Engineering with hands-on experience in projects involving IoT, embedded systems, and Python-based automation.",
+  },
+  {
+    title: "Internship - BSNL Satellite Training",
+    date: "Summer 2020",
+    showDate: "Jun, 2020",
+    description: "Click to Expand",
+    detailedDescription:
+      "Underwent a 15 days-long training at BSNL, where I learned about satellite communication, signal transmission processes, and real-world telecom infrastructure.",
+  },
+];
 
 export const contactInfo = {
   phone: "+91-8793843187 / +91-9082688865",
